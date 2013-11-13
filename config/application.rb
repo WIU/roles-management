023b1039180627779653e90b930dd64cd753eaca
Christopher Thielen <cthielen@gmail.com>
2013-11-13 12:49:07 -0800

    Set locale and time zone explicitly. Removed unused asset path.

--- config/application.rb
+++ config/application.rb
@@ -35,11 +35,11 @@ module DSSRM
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Pacific Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
 
     # JavaScript files you want as :defaults (application.js is always included).
     config.action_view.javascript_expansions[:defaults] = ['jquery']
@@ -59,7 +59,6 @@ module DSSRM
     config.template_version = '1'
 
     if config.template_version == '1'
-      config.assets.paths << Rails.root.join("app", "assets", "v1", "templates")
       config.assets.paths << Rails.root.join("vendor", "assets", "v1", "javascripts")
       config.assets.paths << Rails.root.join("vendor", "assets", "v1", "stylesheets")
     elsif config.template_version == '2'
